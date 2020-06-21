44788cf87f0176d6d024d2c038ea56e82dc1c110
Helloyunho <yunho050840@gmail.com>
2020-06-21 13:39:38 +0900

    Initial Commit

---
+++ Paste-alt/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Paste-alt
+//
+//  Created by Helloyunho on 2020/06/21.
+//  Copyright © 2020 Helloyunho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Paste-altTests/Paste_altTests.swift
@@ -0,0 +1,34 @@
+//
+//  Paste_altTests.swift
+//  Paste-altTests
+//
+//  Created by Helloyunho on 2020/06/21.
+//  Copyright © 2020 Helloyunho. All rights reserved.
+//
+
+import XCTest
+@testable import Paste_alt
+
+class Paste_altTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

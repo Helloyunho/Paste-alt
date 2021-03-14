b61cb89254cea3f9aa05d5af27a6a82f58fb40fb
Helloyunho <yunho050840@gmail.com>
2021-03-14 15:55:51 +0900

    Initial Commit

---
+++ Paste-alt-rewrite/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Paste-alt-rewrite
+//
+//  Created by Helloyunho on 2021/03/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
